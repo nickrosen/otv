49b168efcdde4ce69c5a746c1875a2acd69b018e
Nick Rosen <rosennm@gmail.com>
2024-01-13 17:24:52 -0800

    Initial Commit

---
+++ otv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  otv
+//
+//  Created by Nick Rosen on 1/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ otv/otvApp.swift
@@ -0,0 +1,17 @@
+//
+//  otvApp.swift
+//  otv
+//
+//  Created by Nick Rosen on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct otvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
